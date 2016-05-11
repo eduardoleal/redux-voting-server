e8717f84b65b8bca6e668536806c3e9876942575
Eduardo Leal <me@lealeduardo.com>
2016-05-11 20:29:04 -0300

    voting server

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ export default function startServer(store) {
     const io = new Server().attach(8090);
     store.subscribe(() => io.emit('state', store.getState().toJS()));
 
-    io.on('connect', () => {
+    io.on('connect', (socket) => {
         socket.emit('state', store.getState().toJS());
         socket.on('action', store.dispatch.bind(store));
     });
